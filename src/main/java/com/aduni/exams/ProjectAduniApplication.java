893054e690a0aa8de5a2cfe9a769bbcbe66db481
Sonny Taniguchi <sonny.taniguchi@utp-sistemas.com>
2023-06-13 08:49:54 -0500

    first commit

---
+++ src/main/java/com/aduni/exams/ProjectAduniApplication.java
@@ -0,0 +1,13 @@
+package com.aduni.exams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAduniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAduniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aduni/exams/ProjectAduniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aduni.exams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAduniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
