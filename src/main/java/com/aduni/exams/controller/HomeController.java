3e2568f2cce0635772e840f98be484ef29105c99
Sonny Taniguchi <sonny.taniguchi@utp-sistemas.com>
2023-07-07 08:57:43 -0500

    sprint v1.0

--- src/main/java/com/aduni/exams/controller/HomeController.java
+++ src/main/java/com/aduni/exams/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.aduni.exams.controller;
 
 import com.aduni.exams.entity.Resultado;
 import com.aduni.exams.service.ResultadoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,16 +13,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
+@Slf4j
 @RequestMapping("/home")
 public class HomeController {
     @Autowired
     private ResultadoService resultadoService;
     @GetMapping("/historial")
     public String historial(Model model){
-
-
         List<Resultado> listaResultado = resultadoService.listaResultados(1);
         model.addAttribute("listaResultado",listaResultado);
-        return "home/historial/index";
+        log.info("hola resultado");
+        return "/home/historial/index";
     }
+
+    @GetMapping("/entrada")
+    public String entrada(Model model){
+        return "/home/examenes/medicion";
+    }
+
+    @GetMapping("/generador")
+    public String generador(Model model){
+        return "/home/examenes/generador";
+    }
+
 }
--- src/main/java/com/aduni/exams/entity/Resultado.java
+++ src/main/java/com/aduni/exams/entity/Resultado.java
@@ -3,6 +3,9 @@ package com.aduni.exams.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.sql.Time;
+import java.util.Date;
+
 @Data
 @Entity
 @Table(name = "resultados")
@@ -11,5 +14,10 @@ public class Resultado {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Integer usuario_id;
+    private Double nota;
+    private Character apreciacion;
+    private Date fecha_realizada;
+    private Time tiempo;
+    private String curso;
 
 }
